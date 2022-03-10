a987982b1bc36a67a725699086bc1a24eed9ab77
cubidesrojas <cubideschristian8@gmail.com>
2022-03-10 12:48:31 -0600

    auto commit

---
+++ firstbashprog.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash 
+
+#add and  commit  on git
+#echo  "what you need to do? "
+#read -p "(1)add or (2)commit? :" name
+#if [[ $name == "add" ]]; then
+ # echo "git add "
+#else
+#echo "git commit -m"
+#fi
+
+touch ./textcommit.txt 
+echo ./"HELLO  CHRISTIAN LETS MAKE OUR WORLD BETTER">> ./textcommit.txt
+cp textcommit.txt /c/Users/bigsh/Desktop/revature/christiancubides 
+git add ./
+git commit -m "auto commit "
+git push 
